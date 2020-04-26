d14c3ef3cdca6462070e34a85f9376711c7c2e96
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 18:17:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,18 @@
   # }
   
 def remove_strawberry(contacts)
-
+contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
 end
 
 # contacts.each do |person, contact_details_hash|
