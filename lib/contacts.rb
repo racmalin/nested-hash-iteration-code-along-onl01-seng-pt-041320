bb4d477cacd32d80e2276a421f738f53c2380ae2
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 18:47:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
 def remove_strawberry(contacts)
-# contacts.each do |person, contact_details_hash|
-# binding.pry
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
