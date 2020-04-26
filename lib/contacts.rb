e3347ac6f6431c4ed2994b913792a9c8761ce696
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 18:50:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,24 +4,25 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
       binding.pry   
    end
  end  
-   contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  }
+#   contacts = {
+#     "Jon Snow" => {
+#       name: "Jon",
+#       email: "jon_snow@thewall.we", 
+#       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+#     },
+#     "Freddy Mercury" => {
+#       name: "Freddy",
+#       email: "freddy@mercury.com",
+#       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+#     }
+#   }
 
- end
-end
+# end
+# end
 
 
 
