0508ebedc9b7f4f09233f170538b610d9719ac08
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 18:50:56 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -6,8 +6,9 @@ contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
       binding.pry   
-   end
- end  
+      end
+  end
+ end
 #   contacts = {
 #     "Jon Snow" => {
 #       name: "Jon",
